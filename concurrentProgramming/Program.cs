0f0ffab54005fc861dfa3af98804e7e90cb67f37
miloszwierucki <milosz@wierucki.com>
2024-03-16 18:29:31 +0100

    Complete Stage 0

---
+++ concurrentProgramming/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, sum = 0, r;
+            Console.WriteLine("Enter a Number : ");
+            num = int.Parse(Console.ReadLine());
+            while (num != 0)
+            {
+                r = num % 10;
+                num = num / 10;
+                sum = sum + r;
+            }
+            Console.WriteLine("Sum of Digits of the Number : " + sum);
+            Console.ReadLine();
+
+            string message = "Hello World!!";
+
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+namespace tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int number = 0;
+
+            Assert.AreEqual(0, number);
+        }
+    }
+}
